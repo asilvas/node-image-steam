10528004182277a9e58453120af4245a2fd9617b
Aaron Silvas <asilvas@godaddy.com>
2015-08-07 09:37:43 -0700

    #17. multiple binding support & ssl fix

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,6 +1,11 @@
 var path = require('path');
 
 module.exports = {
+  http: [{
+    port: 13337
+  }, {
+    port: 13338
+  }],
   storage: {
     driver: 'fs',
     path: path.resolve(__dirname, '../test/files')
